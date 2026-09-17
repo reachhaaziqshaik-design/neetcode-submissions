5982a72e94176d714be63e5aa14d22b62d140a39
reachhaaziqshaik-design <reach.haaziqshaik@gmail.com>
2026-09-17 06:24:05 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+
+            return False
+        arr=[0]*26
+        
+        for i in range(len(s)):
+            arr[ord(s[i])-ord('a')] +=1
+            arr[ord(t[i])-ord('a')] -=1
+            
+        for check in arr:
+            if check != 0:
+                return False
+
+        return True
